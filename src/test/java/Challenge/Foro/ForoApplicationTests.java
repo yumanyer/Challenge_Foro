8a3f967f24ef8970016521437b0feb9483c8b34e
yumanyer <manuelpisoni09@gmail.com>
2026-03-04 05:14:57 -0300

    Proyecto seteado

---
+++ src/test/java/Challenge/Foro/ForoApplicationTests.java
@@ -0,0 +1,13 @@
+package Challenge.Foro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
